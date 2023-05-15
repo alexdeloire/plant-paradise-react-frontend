2edc33116efecfc85094ce66bd31ed8a6d2a7ca1
alexandre <alexandre.deloire01@etu.umontpellier.fr>
2023-05-15 19:57:10 +0200

    api url update

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "http://backend-plant-paradise.cluster-ig3.igpolytech.fr/api/auth/";
 
 class AuthService {
   login(username, password) {
