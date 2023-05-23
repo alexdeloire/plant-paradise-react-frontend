a74d538b070e1c9ca4c3b9e4d2378a143ad095a3
alexandre <alexandre.deloire01@etu.umontpellier.fr>
2023-05-23 10:30:38 +0200

    https

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://backend-plant-paradise.cluster-ig3.igpolytech.fr/api/auth/";
+const API_URL = "https://backend-plant-paradise.cluster-ig3.igpolytech.fr/api/auth/";
 //const API_URL = "http://localhost:8080/api/auth/";
 
 
